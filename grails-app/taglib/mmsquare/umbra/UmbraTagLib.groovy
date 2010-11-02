92080002627eea78b706f3c31c495e732b82fa8c
Marcin Floryan <marcin.floryan@gmail.com>
2010-11-02 14:08:35 +0000

    Added a guard

--- grails-app/taglib/mmsquare/umbra/UmbraTagLib.groovy
+++ grails-app/taglib/mmsquare/umbra/UmbraTagLib.groovy
@@ -6,32 +6,32 @@ class UmbraTagLib {
 	static namespace = "umbra"
 
 	def showPicture = { attrs ->
-		if (attrs.picture) {
-			def picture = attrs.remove("picture")
+		def picture = attrs.remove("picture")
+		if (picture && picture.formats) {
 			def linkUrl = pictureFormatForLink(picture).url
 			def imageFormat = pictureFormatForImage(picture)
-			def altText = picture.title?:imageFormat.path.find(/[\w.-]+$/)
+			def altText = picture.title ?: imageFormat?.path.find(/[\w.-]+$/)
 			if (attrs["class"]) attrs["class"] += " fancyboxImage"
 			else attrs["class"] = "fancyboxImage"
 
 			def html = new MarkupBuilder(out)
 			html.a(href: linkUrl, class: attrs["class"]) {
-				img src: imageFormat.url, width: imageFormat.width, height: imageFormat.height, alt: altText
+				img src: imageFormat?.url, width: imageFormat.width, height: imageFormat.height, alt: altText
 			}
-			out						
-		}	
+			out
+		}
 	}
 
 	private def pictureFormatForLink(picture) {
-		formatForType(picture, FormatType.LARGE)?:formatForType(picture, FormatType.ORIGINAL)		
+			formatForType(picture, FormatType.LARGE) ?: formatForType(picture, FormatType.ORIGINAL)
 	}
 
 	private def pictureFormatForImage(picture) {
-		formatForType(picture, FormatType.SMALL)?:formatForType(picture, FormatType.ORIGINAL)
+			formatForType(picture, FormatType.SMALL) ?: formatForType(picture, FormatType.ORIGINAL)
 	}
 
 	private def formatForType(picture, type) {
-		return picture.formats.find {
+		def format = picture.formats.find {
 			it.type == type
 		}
 	}
