a43f4970be4e5f3d30feee191e447fec7f0a5714
Marcin Floryan <marcin.floryan@gmail.com>
2010-11-03 14:00:15 +0000

    Entries can now be paginated.

--- grails-app/controllers/mmsquare/umbra/EntryController.groovy
+++ grails-app/controllers/mmsquare/umbra/EntryController.groovy
@@ -4,19 +4,29 @@ class EntryController {
 
 	def entryService
 
-    def defaultAction = "list"
+	def defaultAction = "list"
 
-    def list = {
-	    [entries: entryService.entries]
-    }
+	def list = { EntryListCommand filterCommand ->
+		[filterCommand: filterCommand, entries: entryService.getEntries(filterCommand)]
+	}
 
-    def show = {
-      def permalink = "/${params?.year}/${params?.month}/${params?.id}"
-      def entry = Entry.findByPermalink(permalink)
-      if (!entry) {
-        redirect action:"list"
-      } else {
-        [entry: entry]
-      }
-    }
+	def show = {
+		def permalink = "/${params?.year}/${params?.month}/${params?.id}"
+		def entry = Entry.findByPermalink(permalink)
+		if (!entry) {
+			redirect action: "list"
+		} else {
+			[entry: entry]
+		}
+	}
+}
+
+class EntryListCommand {
+
+	static final int ENTRIES_PER_PAGE = 3
+	Integer pageNumber = 1
+
+	Integer getOffset() {
+		(pageNumber - 1) * ENTRIES_PER_PAGE
+	}
 }
--- grails-app/services/mmsquare/umbra/EntryService.groovy
+++ grails-app/services/mmsquare/umbra/EntryService.groovy
@@ -1,13 +1,14 @@
 package mmsquare.umbra
 
 import org.joda.time.DateTime
+import grails.orm.PagedResultList
 
 class EntryService {
 
 	static transactional = true
 
-	def getEntries() {
-		Entry.createCriteria().list {
+	PagedResultList getEntries(EntryListCommand filterCommand) {
+		Entry.createCriteria().list([max:filterCommand.ENTRIES_PER_PAGE, offset:filterCommand.offset]) {
 			le("publishDate", new DateTime())
 			order("publishDate", "desc")
 		}
