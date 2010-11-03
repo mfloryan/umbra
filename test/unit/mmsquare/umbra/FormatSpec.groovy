2b4e0ccbc2f7100d4a40dd5613f9d2cffc9d2693
Marcin Floryan <marcin.floryan@gmail.com>
2010-11-03 22:20:25 +0000

    Fixed invalid assertions

--- test/unit/mmsquare/umbra/FormatSpec.groovy
+++ test/unit/mmsquare/umbra/FormatSpec.groovy
@@ -4,84 +4,84 @@ import grails.plugin.spock.UnitSpec
 
 class FormatSpec extends UnitSpec {
 
-  def "Default type of Format is ORIGINAL"() {
-    given:
-    mockDomain Picture
-    def picture = new Picture()
+	def "Default type of Format is ORIGINAL"() {
+		given:
+		mockDomain Picture
+		def picture = new Picture()
 
-    when:
-    def format = new Format(picture: picture)
+		when:
+		def format = new Format(picture: picture)
 
-    then:
-    format.type = FormatType.ORIGINAL
-  }
+		then:
+		format.type == FormatType.ORIGINAL
+	}
 
-  def "Format fails validation with missing properties"() {
-    setup:
-    mockDomain Format
+	def "Format fails validation with missing properties"() {
+		setup:
+		mockDomain Format
 
-    when:
-    def format = new Format()
+		when:
+		def format = new Format()
 
-    then:
-    !format.validate()
-    format.errors.hasFieldErrors("width")
-    format.errors.hasFieldErrors("height")
-    format.errors.hasFieldErrors("path")
-    format.errors.hasFieldErrors("picture")
-  }
+		then:
+		!format.validate()
+		format.errors.hasFieldErrors("width")
+		format.errors.hasFieldErrors("height")
+		format.errors.hasFieldErrors("path")
+		format.errors.hasFieldErrors("picture")
+	}
 
-  def "Format with correct properties set validates"() {
-    setup:
-    mockDomain Picture
-    def picture = new Picture()
-    mockDomain Format
+	def "Format with correct properties set validates"() {
+		setup:
+		mockDomain Picture
+		def picture = new Picture()
+		mockDomain Format
 
-    when:
-    def format = new Format(
-      width: 100, height: 200, path: "/some/path/to/file.JPG", picture: picture
-    )
+		when:
+		def format = new Format(
+				width: 100, height: 200, path: "/some/path/to/file.JPG", picture: picture
+		)
 
-    then:
-    format.validate()
-  }
+		then:
+		format.validate()
+	}
 
-  def "Only one format can exist with a given path"() {
-    given:
-    mockDomain Format
-    mockDomain Picture
-    def picture = new Picture()
+	def "Only one format can exist with a given path"() {
+		given:
+		mockDomain Format
+		mockDomain Picture
+		def picture = new Picture()
 
-    and: "A format exists with some path"
-    def format1 = new Format(width:100, height:200, path: "/some/path/to/file.jpg", picture:picture)
+		and: "a format exists with some path"
+		def format1 = new Format(width: 100, height: 200, path: "/some/path/to/file.jpg", picture: picture)
 
-    when: "Then first format is saved"
-    format1.save(failOnError:true)
+		when: "Then first format is saved"
+		format1.save(failOnError: true)
 
-    and: "New format with the same path is crated"
-    def format2 = new Format(width:200, height: 300, path: "/some/path/to/file.jpg", picture:picture)
+		and: "New format with the same path is crated"
+		def format2 = new Format(width: 200, height: 300, path: "/some/path/to/file.jpg", picture: picture)
 
-    then: "Validation fails for the second format"
-    !format2.validate()
-  }
+		then: "Validation fails for the second format"
+		!format2.validate()
+	}
 
-  def "Format type sets width from hint if hint exists and with is not set"() {
-    given:
-    def picture = new Picture()
+	def "Format type sets width from hint if hint exists and width is not set"() {
+		given:
+		def picture = new Picture()
 
-    when: "A format is created with LARGE type"
-    def format = new Format(height: 200, path:"/file.jpg", picture:picture )
-    format.type = FormatType.LARGE
+		when: "A format is created with LARGE type"
+		def format = new Format(height: 200, path: "/file.jpg", picture: picture)
+		format.type = FormatType.LARGE
 
-    then:
-    format.type == FormatType.LARGE
-    format.width == FormatType.LARGE.formatTypeWidth
+		then:
+		format.type == FormatType.LARGE
+		format.width == FormatType.LARGE.formatTypeWidth
 
-    when: "A format is created with LARGE type and existing width"
-    def format1 = new Format(width: 100, height:200, path:"/file.jpg")
-    format1.type = FormatType.LARGE
-
-    then:
-    format1.width == 100
-  }
+		when: "A format is created with LARGE type and existing width"
+		def format1 = new Format(width: 100, height: 200, path: "/file.jpg")
+		format1.type = FormatType.LARGE
+		
+		then:
+		format1.width == 100
+	}
 }
