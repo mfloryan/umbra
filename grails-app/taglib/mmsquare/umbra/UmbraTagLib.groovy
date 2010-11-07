0c9b06d7f4075e47b149c4416aba4e1c8e27e442
Marcin Floryan <marcin.floryan@gmail.com>
2010-11-07 22:07:00 +0000

    And this silly script messes up with my files...eh.

--- grails-app/taglib/mmsquare/umbra/UmbraTagLib.groovy
+++ grails-app/taglib/mmsquare/umbra/UmbraTagLib.groovy
@@ -12,7 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */age mmsquare.umbra
+ */
+
+package mmsquare.umbra
 
 import groovy.xml.MarkupBuilder
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
