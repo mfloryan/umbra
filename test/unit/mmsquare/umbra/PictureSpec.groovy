9e9a72e08fbb12b0b8db5663584550c018e24939
Marcin Floryan <marcin.floryan@gmail.com>
2010-11-08 09:01:46 +0000

    Get required format types for image

--- test/unit/mmsquare/umbra/PictureSpec.groovy
+++ test/unit/mmsquare/umbra/PictureSpec.groovy
@@ -1,7 +1,24 @@
+/*
+ * Copyright (c) 2010 Marcin Floryan. http://www.mmsquare.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package mmsquare.umbra
 
 import grails.plugin.spock.UnitSpec
 import org.joda.time.DateTime
+import static mmsquare.umbra.FormatType.*
 
 class PictureSpec extends UnitSpec {
 
@@ -14,5 +31,22 @@ class PictureSpec extends UnitSpec {
 
 		then:
 		p.validate()
+	}
+
+	def "picture provides list of required FORMATS given original's width"() {
+		when:
+		def types = Picture.getFormatTypesForWidth(width)
+
+		then:
+		types == formatTypes
+
+		where:
+		width | formatTypes
+		10    | []
+		120   | [THUMBNAIL]
+		640   | [THUMBNAIL]
+		800   | [SMALL, THUMBNAIL]
+		1200  | [SMALL, THUMBNAIL]
+		1400  | [LARGE, SMALL, THUMBNAIL ]
 	}
 }
